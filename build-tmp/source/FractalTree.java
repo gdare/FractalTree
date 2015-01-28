e1af83ab64a7f65fb73aecebd6eb1fb031d0599f
gdare <gregory.dare27@gmail.com>
2015-01-28 10:05:35 -0800

    leaf

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 5; 
-private double branchAngle = 0.5;  
+private double branchAngle = mouseX *0.1f;  
 public void setup() 
 {   
 	size(640,480);    
@@ -32,3 +48,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
     }
     
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
